4dd27cd6117cc7c5189664c309301130d10742eb
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-09-02 11:26:44 +0530

    book apu

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+const { response } = require("express");
 const express  = require("express");
 
+//database
+const Database = require("./database")
+
+//Initialization
 const OurApp = express()
 
 OurApp.get("/", (request, response) =>
 {
     response.json({
-        message: "Request Served!!!!"
+        message: "server is working!!!"
     })
 })
 
+OurApp.get("/book", (req,res) => {
+   return res.json({
+        books: Database.Book       
+    });
+});
 OurApp.listen(5000, () => console.log("server is running"))
